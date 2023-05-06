e0fa190be5e28345b8141dbc1fa1e588f4f2a935
Richard Wei <48197502+richardwei6@users.noreply.github.com>
2023-05-05 23:57:52 -0700

    Update w req-rep messaging

--- sketch_may05b/sketch_may05b.ino
+++ sketch_may05b/sketch_may05b.ino
@@ -15,7 +15,7 @@ struct can_frame frame;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (mcp2515.readMessage(&frame) == MCP2515::ERROR_OK){
+  /*if (mcp2515.readMessage(&frame) == MCP2515::ERROR_OK){
     Serial.print("recieved can msg -> ");
     Serial.print("id = ");
     Serial.print(frame.can_id);
@@ -26,6 +26,45 @@ void loop() {
   }
   else{
     Serial.println("no msg");
+  }*/
+
+  frame = can_frame();
+
+  //
+
+  frame.can_id = 1;
+  frame.can_dlc = 1;
+  frame.data[0] = 1;
+
+  mcp2515.sendMessage(&frame);
+  Serial.println("sent req frame");
+
+  //
+
+  frame = can_frame();
+
+  //
+
+  int t = 0;
+  while (t < 100){
+    if (mcp2515.readMessage(&frame) == MCP2515::ERROR_OK && frame.can_id == 2){
+        Serial.print("recieved success can frame -> ");
+        Serial.print("id = ");
+        Serial.print(frame.can_id);
+        Serial.print("; length = ");
+        Serial.print(frame.can_dlc);
+        Serial.print("; data = ");
+        Serial.println(frame.data[0]);
+        goto recvmsg;
+    }
+    t++;
+    delay(50);
   }
-  delay(20);
+
+  // no msg recv
+  Serial.println("failed to recv success frame");
+  return;
+
+  recvmsg:
+  delay(1000);
 }
